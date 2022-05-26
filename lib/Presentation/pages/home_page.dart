d46eff76c9ccb94aba26ac9090eb12d50dd63fdd
tiancris01 <tiancris01@gmail.com>
2022-05-26 09:41:28 -0500

    TabBar

--- lib/Presentation/pages/home_page.dart
+++ lib/Presentation/pages/home_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:zemoga_test/Domain/entities/post/post_model.dart';
-import 'package:zemoga_test/Domain/usecases/post/post_usecase.dart';
 
 import '../../Config/provider/providers.dart';
-import '../widgets/future_biulder.dart';
+import '../../Domain/entities/post/post_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,28 +11,95 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // final providerLocation = Provider.of<LocationProvider>(context);
     final providerPost = Provider.of<PostProvider>(context);
-    return Scaffold(
-        appBar: AppBar(title: const Text('User App Clean Arquitecture')),
-        body: FutureBuilder<List<PostModel>>(
-            future: providerPost.postUsecase.getAllPost(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: 5,
-                    itemBuilder: ((context, index) {
-                      PostModel post = snapshot.data![index];
-                      return Text(post.title);
-                    }));
-              } else if (snapshot.hasError) {
-                return Text('$snapshot.hasError');
-              }
-              return const Center(
-                child: SizedBox(
-                  width: 20,
-                  child: CircularProgressIndicator(),
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('LIST POST'),
+            centerTitle: true,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  child: Text('All'),
                 ),
-              );
-            }));
+                Tab(
+                  child: Text('Favorite'),
+                )
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              FutureBuilder<List<PostModel>>(
+                  future: providerPost.postUsecase.getAllPost(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: ((context, index) {
+                            PostModel post = snapshot.data![index];
+                            return Card(
+                              color: Colors.blueGrey[100],
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    child: Text(
+                                      '${post.id}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                title: Text(post.title),
+                                trailing: const Icon(Icons.star),
+                              ),
+                            );
+                          }));
+                    } else if (snapshot.hasError) {
+                      return Text('$snapshot.hasError');
+                    }
+                    return const Center(
+                      child: SizedBox(
+                        width: 20,
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }),
+              FutureBuilder<List<PostModel>>(
+                  future: providerPost.postUsecase.getAllPost(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: ((context, index) {
+                            PostModel post = snapshot.data![index];
+                            return Card(
+                              color: Colors.blueGrey[100],
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    child: Text(
+                                      '${post.id}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                                title: Text(post.title),
+                                trailing: const Icon(Icons.star),
+                              ),
+                            );
+                          }));
+                    } else if (snapshot.hasError) {
+                      return Text('$snapshot.hasError');
+                    }
+                    return const Center(
+                      child: SizedBox(
+                        width: 20,
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }),
+            ],
+          )),
+    );
 
     /* FutureBuilder<Location>(
             future: providerLocation.locationUsecase.getAllLocations(),
